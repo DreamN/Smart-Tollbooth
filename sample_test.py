5bef937164a02b3d947b741fd5d27f8512e13a36
Isara Naranirattisai <isaradream@gmail.com>
2017-03-25 13:14:53 +0700

    test: Add database test

---
+++ sample_test.py
@@ -0,0 +1,14 @@
+from app import *
+
+def insertCar():
+    addCar('SX9273', 'Peter Quill', '18jsabD8ahdmw9xoamdjsTz')
+    addCar('TA2837', 'Stephen Strange', 'wmzidnvSzhdikwlskaLsisj')
+    addCar('AP2843', 'Tony Stark', 'SuxannsFcsjFxkxahiduenf')
+    addCar('ML2837', 'Blackagar Boltagon', 'WlsmjfihnsA8xh3nf92dnks')
+    addCar('MG2831', 'Thor Odinson', 'Azhw7dSxuujw9d8u4jg9dj5')
+    addCar('AH3928', 'Peter Parker', 'Ss9tB9cjrmgo89n4igue8fj')
+    addCar('SY8347', 'Daniel Rand', 'E9cjnAusjwwksa84h8htjt8')
+
+printInfo()
+insertCar()
+printInfo()
