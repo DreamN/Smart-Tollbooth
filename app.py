8971cf5fd64d01e333cab0d68ec2bdf01b1a5961
Isara Naranirattisai <isaradream@gmail.com>
2017-03-25 12:50:15 +0700

    feat: Function for car and parking

--- app.py
+++ app.py
@@ -12,7 +12,37 @@ app = Flask(__name__)
 #+-----------------------------------------------------+#
 #|                 Function Declaration                +#
 #+-----------------------------------------------------+#
+def printInfo():
+    car_in_parking = session.query(CarInParking)
+    print 'There\'s a %d car in parking.' % len(car_in_parking)
 
+def insertCarParking(car_id):
+    car = session.query(Car).filter_by(id = car_id).one()
+    newCar = CarInParking(car = car)
+    session.add(newCar)
+    session.commit()
+    print 'Inserted %s to the parking' % car.id
+
+def removeCarParking(car_id):
+    deleteCar = session.query(CarInParking).filter_by(car_id = car_id).one()
+    session.delete(deleteCar)
+    session.commit()
+    print '%s is removed from the parking' % car.id
+
+def printCarList():
+    car_list = session.query(Car)
+    print '<================================>'
+    print 'There\'s a %d car in the system.' % len(car_list)
+    for car in car_list:
+        print '%s %s %s' % (car.id, car.owner, car.rfid_id)
+
+def addCar(car_id, car_owner, car_rfid_id):
+    try:
+        newCar = Car(id = car_id, owner = car_owner, rfid_id = car_rfid_id)
+        session.add(newCar)
+        session.commit()
+    except:
+        print 'Can\'t add this car....\n\t error happens'
 
 #+-----------------------------------------------------+#
 #|                     Flask's View                    +#
