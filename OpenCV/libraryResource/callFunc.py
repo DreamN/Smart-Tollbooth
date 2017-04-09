3d2471e2e00a186677919cef38b7a1c1269563f4
Peachman05 <patcharapon1995@gmail.com>
2017-04-09 22:09:01 +0700

    Add callFunc

---
+++ OpenCV/libraryResource/callFunc.py
@@ -0,0 +1,9 @@
+
+# import sys
+# sys.path.insert(0, '/path/to/application/app/folder')
+
+from MainFunc import main
+
+nameInput = "LicPlateImages\\use01.jpg"
+nameOutput = "Output\\imgDrawRec.jpg"
+print(main(nameInput,nameOutput))
