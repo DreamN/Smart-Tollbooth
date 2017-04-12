ec09360c8de60955fd54b6584f039e43ec8f7257
Isara Naranirattisai <isaradream@gmail.com>
2017-04-12 21:07:40 +0700

    cleam: Remove unnecessary file/dirs

--- somesetting/GRANT_DB.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE smarttb;
-CREATE USER pjuser WITH PASSWORD 'random';
-ALTER ROLE pjuser SET client_encoding TO 'utf8';
-ALTER ROLE pjuser SET default_transaction_isolation TO 'read committed';
-ALTER ROLE pjuser SET timezone TO 'Asia/Bangkok';
-GRANT ALL PRIVILEGES ON DATABASE smarttb TO pjuser;
